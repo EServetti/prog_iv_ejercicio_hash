ca3287254086ef227ba4789e9f82d223e561678f
unknown <eservetti2018@gmail.com>
2026-04-08 09:32:06 -0300

    Finalizado ejercicio

---
+++ src/main/java/com/eservetti/ejercicio_hashing/EjercicioHashingApplication.java
@@ -0,0 +1,45 @@
+package com.eservetti.ejercicio_hashing;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.bouncycastle.jcajce.provider.digest.MD5;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class EjercicioHashingApplication {
+    private static void testHashBCrypt(String password, int strength) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(strength);
+        long start = System.currentTimeMillis();
+        String hashedPassword = encoder.encode(password);
+        long end = System.currentTimeMillis();
+
+        System.out.println("Hash Password: " + hashedPassword);
+        System.out.println("Hash Strength: " + strength);
+        System.out.println("Time: " + (end - start) + " ms");
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioHashingApplication.class, args);
+        int hashLen = 12;
+        String rawPassword = "utn_2026";
+        System.out.println("Contraseña sin modificaciones: " + rawPassword);
+        System.out.println("Longitud del hash: " + hashLen);
+        // Hashing por MD5
+        String hash1 = DigestUtils.md5Hex(rawPassword);
+        String hash2 = DigestUtils.md5Hex(rawPassword);
+
+        System.out.println("Hash1: " + hash1);
+        System.out.println("Hash2: " + hash2);
+        System.out.println("Longitud del hash: " + hashLen);
+        // Hashing por BCrypt
+        testHashBCrypt(rawPassword, 4);
+        testHashBCrypt(rawPassword, 10);
+        testHashBCrypt(rawPassword, 15);
+        // Hashing por Argon
+        Argon2PasswordEncoder argonEncoder = new Argon2PasswordEncoder(16, hashLen,1, 65536,3);
+        System.out.println("Argon hash: " + argonEncoder.encode(rawPassword));
+	}
+
+}
---
+++ src/test/java/com/eservetti/ejercicio_hashing/EjercicioHashingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eservetti.ejercicio_hashing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjercicioHashingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
